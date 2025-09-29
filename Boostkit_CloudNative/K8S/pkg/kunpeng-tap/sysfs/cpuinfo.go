7ce991c5bc6df96f81b10b36f3d35d466acd2138
fsq0089 <fengshangqing@huawei.com>
2025-09-29 06:51:40 +0000

    !95 correct copyright for Boostkit_CloudNative/K8S/pkg/kunpeng-tap/sysfs/cpuinfo.go * correct copyright for Boostkit_CloudNative/K8S/pkg/kunpeng-tap/sysfs/cpuinfo.go

--- Boostkit_CloudNative/K8S/pkg/kunpeng-tap/sysfs/cpuinfo.go
+++ Boostkit_CloudNative/K8S/pkg/kunpeng-tap/sysfs/cpuinfo.go
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2025 Huawei Technology corp.
+ * Copyright 2022 The Koordinator Authors.
+ * Modifications Copyright 2025 Huawei Technology corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
