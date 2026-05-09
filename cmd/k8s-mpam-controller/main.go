ac284055b19d54303b0b0dc6a50abe3eefebbca8
yanxiang <1370277873@qq.com>
2026-05-09 16:12:53 +0800

    main: wire manager startup with reconciler and dynamiccontrol components

--- cmd/k8s-mpam-controller/main.go
+++ cmd/k8s-mpam-controller/main.go
@@ -18,9 +18,133 @@
 package main
 
 import (
-	"kunpeng.huawei.com/kunpeng-cloud-computing/pkg/k8s-mpam-controller/agent"
+	"flag"
+	"os"
+	"time"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	ctrlconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	qosv1alpha1 "kunpeng.huawei.com/kunpeng-cloud-computing/api/k8s-mpam-controller/v1alpha1"
+	"kunpeng.huawei.com/kunpeng-cloud-computing/pkg/k8s-mpam-controller/controller"
+	"kunpeng.huawei.com/kunpeng-cloud-computing/pkg/k8s-mpam-controller/dynamiccontrol"
 )
 
 func main() {
-	agent.Main()
+	klog.InitFlags(nil)
+
+	runLocal := flag.Bool("run-local", false, "run controller out of cluster for local debugging")
+	enableDynamicControl := flag.Bool("enable-dynamic-control", false, "enable dynamic interference control loops")
+	agentAddr := flag.String("dynamic-agent-addr", "http://127.0.0.1:18080", "dynamic-control agent address, e.g. http://127.0.0.1:18080")
+	publishInterval := flag.Duration("dynamic-publish-interval", 30*time.Second, "interval for publishing online pod cgroups")
+	applyInterval := flag.Duration("dynamic-apply-interval", 30*time.Second, "interval for pulling interference and applying tuning decisions")
+	taskTimeout := flag.Duration("dynamic-task-timeout", 10*time.Second, "timeout for one dynamic-control task execution")
+	flag.Parse()
+	defer klog.Flush()
+	ctrl.SetLogger(klogr.New())
+
+	cfg, err := buildRESTConfig(*runLocal)
+	if err != nil {
+		klog.Errorf("failed to build kube config: %v", err)
+		os.Exit(1)
+	}
+
+	scheme := runtime.NewScheme()
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(qosv1alpha1.AddToScheme(scheme))
+
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:                 scheme,
+		Metrics:                metricsserver.Options{BindAddress: "0"},
+		HealthProbeBindAddress: ":8081",
+		LeaderElection:         false,
+		LeaderElectionID:       "k8s-mpam-controller.kunpeng.huawei.com",
+	})
+	if err != nil {
+		klog.Errorf("failed to create manager: %v", err)
+		os.Exit(1)
+	}
+
+	nodeIdentity := controller.NewDefaultNodeIdentity()
+	if nodeIdentity.NodeName() == "" {
+		klog.Error("NODE_NAME is empty, please set NODE_NAME env")
+		os.Exit(1)
+	}
+
+	resctrlMgr, err := controller.NewLocalResctrlGroupManager()
+	if err != nil {
+		klog.Errorf("failed to initialize local resctrl manager: %v", err)
+		os.Exit(1)
+	}
+
+	if err := (&controller.QoSPolicyReconciler{
+		Client:       mgr.GetClient(),
+		Scheme:       mgr.GetScheme(),
+		NodeIdentity: nodeIdentity,
+		Resctrl:      resctrlMgr,
+	}).SetupWithManager(mgr); err != nil {
+		klog.Errorf("failed to setup QoSPolicyReconciler: %v", err)
+		os.Exit(1)
+	}
+
+	if err := controller.NewPodBindingReconciler(
+		mgr.GetClient(),
+		mgr.GetScheme(),
+		*enableDynamicControl,
+	).SetupWithManager(mgr); err != nil {
+		klog.Errorf("failed to setup PodBindingReconciler: %v", err)
+		os.Exit(1)
+	}
+
+	if *enableDynamicControl {
+		agentClient := dynamiccontrol.NewTCPHTTPAgentClient(*agentAddr)
+		agentClient.HTTPClient.Timeout = *taskTimeout
+
+		policyUpdater := dynamiccontrol.NewQoSPolicyDynamicUpdater(mgr.GetClient())
+		tuningEngine := dynamiccontrol.NewReasonDispatchTuningEngine(policyUpdater)
+		dynamicCoordinator := dynamiccontrol.NewCoordinator(
+			nodeIdentity,
+			dynamiccontrol.NewLocalOnlinePodSource(mgr.GetClient()),
+			agentClient,
+			tuningEngine,
+		)
+		scheduler := dynamiccontrol.NewSyncScheduler(dynamicCoordinator)
+		scheduler.PublishInterval = *publishInterval
+		scheduler.ApplyInterval = *applyInterval
+		scheduler.TaskTimeout = *taskTimeout
+
+		if err := mgr.Add(scheduler); err != nil {
+			klog.Errorf("failed to add dynamic-control scheduler: %v", err)
+			os.Exit(1)
+		}
+		klog.Infof(
+			"dynamic-control enabled, agent=%s publishInterval=%s applyInterval=%s timeout=%s",
+			*agentAddr, *publishInterval, *applyInterval, *taskTimeout,
+		)
+	}
+
+	klog.Info("starting k8s-mpam-controller manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		klog.Errorf("manager exited with error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func buildRESTConfig(runLocal bool) (*rest.Config, error) {
+	if runLocal {
+		// Reuse controller-runtime/client-go built-in kubeconfig handling.
+		// Use --kubeconfig (already registered by controller-runtime) when needed.
+		klog.Info("running in local debug mode, using controller-runtime kubeconfig resolution")
+		return ctrlconfig.GetConfig()
+	}
+	return rest.InClusterConfig()
 }
